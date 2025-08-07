335315894f2a28abddaee73adb793badf7814b3b
elijom49 <120235228+elijom49@users.noreply.github.com>
2025-08-07 14:34:51 -0400

    Update index.js to app not vericid app

--- 264cefd3-b6b0-443c-937c-08473250d69b/src/index.js
+++ 264cefd3-b6b0-443c-937c-08473250d69b/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import VericidApp from './VericidApp'; // or './App' if you're keeping the original name
+import VericidApp from './App'; // or './App' if you're keeping the original name
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
